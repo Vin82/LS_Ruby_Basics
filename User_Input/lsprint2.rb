6884cbebd24687de603553add451da03f3a887e7
Vincenzo Altieri <vin.altieri@gmail.com>
2016-12-28 21:37:59 -0500

    lsprint2.rb completed

--- User_Input/lsprint2.rb
+++ User_Input/lsprint2.rb
@@ -1,15 +1,21 @@
 # lsprint2.rb
 
-number_of_lines = nil
-to_quit = nil
-
 loop do
+  number_of_lines = nil
+  input = nil
+
   loop do
     puts '>> How many output lines do you want? Enter a number >= 3 (Q to quit):'
-    number_of_lines = gets.to_i
+    input = gets.chomp.downcase
+    break if input == 'q'
+
+    number_of_lines = input.to_i
     break if number_of_lines >= 3
+
     puts ">> That's not enough lines."
   end
+
+  break if input == 'q'
 
   while number_of_lines > 0
     puts 'Launch School is the best!'
